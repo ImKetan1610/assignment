d4d3bdbb2bed85ebb8f5533d9ab4d9a228141c73
ImKetan1610 <96099100+ImKetan1610@users.noreply.github.com>
2022-04-03 23:48:41 +0530

    Add files via upload

---
+++ EXPRESS AUTHENTICATION/src/server.js
@@ -0,0 +1,12 @@
+
+const app = require("./index");
+const connect = require("./configs/db")
+
+app.listen("1610", async () => {
+  try {
+    await connect();
+    console.log("listening on port 1610");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
